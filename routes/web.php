b2ec5d20df391d36a702fbcd2efeb8e0a89b6704
DESKTOP-4QHJ5NU\Zasrok <zeeideveloper19@gmail.com>
2026-01-06 03:59:07 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaporanController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LaporanController::class, 'index']); // Halaman Utama
+Route::get('/export-excel', [LaporanController::class, 'exportExcel'])->name('export.excel');
